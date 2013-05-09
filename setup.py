f7db95b95db48e0ecab6983da1851fead7a59762
Aaron Seilis <aaron.seilis@gmail.com>
2013-05-09 12:25:26 -0400

    Version 0.2
    Official bump to version 0.2. This version is the first release version
    and the project should still be considered in heavy development. Expect
    that any APIs that are currently available will probably change in
    future releases.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
 	name='Microwave',
-	version='0.1',
+	version='0.2',
 	author='Aaron Seilis',
 	author_email='aaron.seilis@gmail.com',
 	packages=['microwave'],
